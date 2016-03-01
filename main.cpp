eacfa06574dc854b255016164ec88b42371ec3ea
Emilio Borges <emilio.j.borges@gmail.com>
2016-03-01 10:32:59 -0500

    Init proj file

---
+++ main.cpp
@@ -0,0 +1,15 @@
+
+
+
+
+#include <iostream>		// cin, cout
+#include <iomanip>		// setw();
+#include <string>
+#include <vector>
+#include <algorithm>	// sort(); to sort intVectorFrequencyTable
+
+using namespace std;
+
+main(){
+	cout << "HI!";
+}
