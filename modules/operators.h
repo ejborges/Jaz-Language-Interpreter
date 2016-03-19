1ebb31a64267bfb2ec66d9e0a3d8a71c96f8ecf3
MSchmitz14 <mschmitz14@aol.com>
2016-03-19 19:20:30 -0400

    Removed useless info XD

--- modules/operators.h
+++ modules/operators.h
@@ -1,7 +1,3 @@
-//
-// Created by Emilio Borges on 3/19/16.
-//
-
 #ifndef JAZ_INTERPRETER_OPERATORS_H
 #define JAZ_INTERPRETER_OPERATORS_H
 
@@ -14,15 +10,14 @@ void div();
 void mod();
 void bitwise_and();
 void bitwise_bang();
-void add();
-void add();
-void add();
-void add();
-void add();
-void add();
-void add();
-void add();
-void add();
+void bitwise_or();
+void not_equal();
+void less_than_or_equal();
+void greater_than_or_equal();
+void less_than();
+void greater_than();
+void equal();
+
 private:
 
 };
