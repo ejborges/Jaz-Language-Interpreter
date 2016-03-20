09aaf51b56477074f43f753a7f9e0573b69a1576
Emilio Borges <emilio.j.borges@gmail.com>
2016-03-20 16:51:58 -0400

    moved global variables to working_data.h and working_data.cpp

---
+++ modules/working_data.cpp
@@ -0,0 +1,46 @@
+/* This is the data types library for the Jaz language interpreter
+ *
+ * ----------------------------------------------------------
+ *
+ *      Authors: Emilio Borges, Marcus Schmitz
+ *      Developed: March 2016
+ *
+ *      The University of Toledo
+ *      EECS 3550: Software Engineering
+ *
+ *      Project files can be found at:
+ *      https://drive.google.com/open?id=0B4CF__kbczDjaEdoRkxqdkg5cXc
+ *
+ *      Project documentation can be found at:
+ *      https://drive.google.com/open?id=0B4CF__kbczDjdWplcVJKTjYwbVczQVVFTkxhMDNzWUVpbGE0
+ *
+ *      Jaz language instructions can be found at:
+ *      https://drive.google.com/open?id=0B4CF__kbczDjdjZ2aTFJTEJ6eUE
+ *
+ *      Textual Design Notation (TDN) document can be found at:
+ *      https://drive.google.com/open?id=1Kn2HFw5_qydebZM2q2Mb6eNdbSVzJNHYhT4-36sdS2E
+ *
+ *
+ *      HOW TO USE ------------------------------------------
+ *
+ *          Include this library's .h file in the interpreter's .h file.
+ *          Then, call any of these data types within the interpreter's
+ *          .cpp file while also passing it the appropriate parameters.
+ *
+ *      HOW IT WORKS ----------------------------------------
+ *
+ *
+ */
+
+#include "working_data.h"
+
+using namespace std;
+
+string file;
+string filename;
+string *file_lines;
+
+string instruction;
+string parameter;
+int line_number;
+int scope_level;
